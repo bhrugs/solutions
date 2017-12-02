f14c369e250bfc737994cfa701243806260f4ab1
bhrugs <bhrugu.code@gmail.com>
2017-12-02 18:29:22 -0500

    complete but, still need to add common code in function

--- p6.js
+++ p6.js
@@ -7,20 +7,110 @@ instead you will give a string with the operation as an input
 i.e. if it receives 5+4-2 the result printed on screen is 7
 */
 
-const EXPRESSION = "6*5+3";
-var ops = ["/", "*", "+", "-"];
-var pos = ops.map(function(element){
-    return EXPRESSION.indexOf(element);
-});
+const EXPRESSION = "6*5+33+3/3";
+
+var ops = [];
+var numbers = [];
+var num = "" ;
+var result;
 
 //only ops in expression
-for (var i = 0; i < pos.length; i++) {
-    if(pos[i] <= 0) {
-        ops.splice(i, 1);
-        pos.splice(i, 1);
+//prepare array for operation
+for (var i = 0; i < EXPRESSION.length; i++) {
+    if (EXPRESSION.charAt(i) == "*" 
+        || EXPRESSION.charAt(i) == "+" 
+        || EXPRESSION.charAt(i) == "-" 
+        || EXPRESSION.charAt(i) == "/") 
+    {
+        ops.push(EXPRESSION.charAt(i));
+    } else {
+        num = num + EXPRESSION.charAt(i);
+        if (parseInt(EXPRESSION.charAt(i+1))) {
+        } else {
+            numbers.push(parseInt(num));
+            num = "";
+        }
     }
-    
 }
-
-console.log(ops);
-console.log(pos);
+// console.log("numbers: "+numbers);
+// console.log("ops: "+ops);
+while(ops.length) {
+    if (ops.indexOf("/") >= 0) {
+        //console.log("division found");
+        if (ops.indexOf("*") >= 0) {
+            //console.log("division and multiplication both found - check what comes first left to right");
+            //check what comes first from left to right
+            if (ops.indexOf("/") < ops.indexOf("*")) {
+                //division comes first from left
+                result = numbers[ops.indexOf("/")] / numbers[ops.indexOf("/")+1];
+                numbers[ops.indexOf("/")] = result;
+                numbers.splice(ops.indexOf("/")+1, 1)
+                ops.splice(ops.indexOf("/"), 1);
+                continue;
+            } else {
+                //multiplication comes first from left
+                result = numbers[ops.indexOf("*")] * numbers[ops.indexOf("*")+1];
+                numbers[ops.indexOf("*")] = result;
+                numbers.splice(ops.indexOf("*")+1, 1)
+                ops.splice(ops.indexOf("*"), 1);
+                continue;
+            }
+        } else {
+            //console.log("do the division first, because no multiplocation");
+            result = numbers[ops.indexOf("/")] / numbers[ops.indexOf("/")+1];
+            numbers[ops.indexOf("/")] = result;
+            numbers.splice(ops.indexOf("/")+1, 1)
+            ops.splice(ops.indexOf("/"), 1);
+            continue;
+        }
+    } else {
+        //console.log("no division");
+        if (ops.indexOf("*") >= 0) {
+            //console.log("do multiplication first");
+            result = numbers[ops.indexOf("*")] * numbers[ops.indexOf("*")+1];
+            numbers[ops.indexOf("*")] = result;
+            numbers.splice(ops.indexOf("*")+1, 1)
+            ops.splice(ops.indexOf("*"), 1);
+            continue;
+        } else {
+            //console.log("do addition and subtraction - from left to right");
+            if (ops.indexOf("+") >= 0) {
+                if (ops.indexOf("-") >= 0) {
+                    //check what comes first from left to right
+                    if (ops.indexOf("+") < ops.indexOf("-")) {
+                        //addition comes first from left
+                        result = numbers[ops.indexOf("+")] + numbers[ops.indexOf("+")+1];
+                        numbers[ops.indexOf("+")] = result;
+                        numbers.splice(ops.indexOf("+")+1, 1)
+                        ops.splice(ops.indexOf("+"), 1);
+                        continue;
+                    } else {
+                        //subtraction comes first from left
+                        result = numbers[ops.indexOf("-")] - numbers[ops.indexOf("-")+1];
+                        numbers[ops.indexOf("-")] = result;
+                        numbers.splice(ops.indexOf("-")+1, 1)
+                        ops.splice(ops.indexOf("-"), 1);
+                        continue;
+                    }
+                } else {
+                    //do addition now
+                    //console.log("no subtraction - do addition now");
+                    result = numbers[ops.indexOf("+")] + numbers[ops.indexOf("+")+1];
+                    numbers[ops.indexOf("+")] = result;
+                    numbers.splice(ops.indexOf("+")+1, 1)
+                    ops.splice(ops.indexOf("+"), 1);
+                    continue;
+                }
+            } else {
+                //do subtraction now
+                //console.log("do subtraction now");
+                result = numbers[ops.indexOf("-")] - numbers[ops.indexOf("-")+1];
+                numbers[ops.indexOf("-")] = result;
+                numbers.splice(ops.indexOf("-")+1, 1)
+                ops.splice(ops.indexOf("-"), 1);
+                continue;
+            }
+        }
+    }
+}
+console.log(EXPRESSION+" = "+numbers.toString());
